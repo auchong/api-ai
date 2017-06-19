7e5795838cb905855e25af4b6bfc19d6c8c04a79
Austin Chong <chong.austin@gmail.com>
2017-06-19 15:36:56 -0400

    Update index.js

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ function getPerDiemRate (args) {
 function returnPerDiemRate (results) {
    var data = JSON.parse(results);
    console.log(data);
-   if (data.result.records[0].Meals) {
+   if (data.result.records.length > 0) {
        var mealRate = data.result.records[0].Meals;
    }
    
